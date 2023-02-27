7d84955166ab56bb0d553aaf1215fb649cbf6d50
WinCodeX <glenwinterg970@gmail.com>
2023-02-27 18:57:52 +0300

    Added Faker

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   resources :payments
   resources :students
-  get 'home/index'
+  root "home#index"
+  get "about", to: "home#about"
+  get "contact", to: "home#contact"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
